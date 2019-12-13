564ffc4a4894be40403f3675a284ff3a4113c7a5
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2019-12-13 15:21:08 +0200

    cosmetic

--- src/UiPath.CoreIpc/NamedPipe/NamedPipeListener.cs
+++ src/UiPath.CoreIpc/NamedPipe/NamedPipeListener.cs
@@ -58,4 +58,14 @@ namespace UiPath.CoreIpc.NamedPipe
             void FullControlFor(WellKnownSidType sid) => pipeSecurity.Allow(sid, PipeAccessRights.FullControl);
         }
     }
+    public static class NamedPipeServiceExtensions
+    {
+        public static ServiceHostBuilder UseNamedPipes(this ServiceHostBuilder builder, NamedPipeSettings settings)
+        {
+            settings.ServiceProvider = builder.ServiceProvider;
+            settings.Endpoints = builder.Endpoints;
+            builder.AddListener(new NamedPipeListener(settings));
+            return builder;
+        }
+    }
 }
