ed1940c5e1e95ecf4277fd5ee01efdfbbdcdabc5
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2021-05-25 10:11:19 +0300

    w

--- src/UiPath.CoreIpc/Server/Listener.cs
+++ src/UiPath.CoreIpc/Server/Listener.cs
@@ -120,7 +120,6 @@ namespace UiPath.CoreIpc
         }
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
