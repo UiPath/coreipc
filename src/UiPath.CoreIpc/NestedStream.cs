c8ccf41953f99baa61e33bd94217f25f487d2f30
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2021-07-25 08:02:17 +0300

    cosmetic

--- src/UiPath.CoreIpc/NestedStream.cs
+++ src/UiPath.CoreIpc/NestedStream.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace UiPath.CoreIpc
 {
     /// <summary>
@@ -14,17 +13,14 @@ namespace UiPath.CoreIpc
         /// The stream to read from.
         /// </summary>
         private readonly Stream _underlyingStream;
-
         /// <summary>
         /// The total length of the stream.
         /// </summary>
         private readonly long _length;
-
         /// <summary>
         /// The remaining bytes allowed to be read.
         /// </summary>
         private long _remainingBytes;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="NestedStream"/> class.
         /// </summary>
@@ -36,111 +32,68 @@ namespace UiPath.CoreIpc
             _remainingBytes = length;
             _length = length;
         }
-
         public event EventHandler Disposed;
-
         /// <inheritdoc />
         public bool IsDisposed { get; private set; }
-
         /// <inheritdoc />
         public override bool CanRead => !IsDisposed;
-
         /// <inheritdoc />
         public override bool CanSeek => !IsDisposed && _underlyingStream.CanSeek;
-
         /// <inheritdoc />
         public override bool CanWrite => false;
-
         /// <inheritdoc />
-        public override long Length
-        {
-            get
-            {
-                return _underlyingStream.CanSeek ?
-                    _length : throw new NotSupportedException();
-            }
-        }
-
+        public override long Length => _underlyingStream.CanSeek ? _length : throw new NotSupportedException();
         /// <inheritdoc />
         public override long Position
         {
-            get
-            {
-                return _length - _remainingBytes;
-            }
-
-            set
-            {
-                Seek(value, SeekOrigin.Begin);
-            }
+            get => _length - _remainingBytes;
+            set => Seek(value, SeekOrigin.Begin);
         }
-
         /// <inheritdoc />
         public override void Flush() => throw new NotSupportedException();
-
         /// <inheritdoc />
         public override Task FlushAsync(CancellationToken cancellationToken) => throw new NotSupportedException();
-
         /// <inheritdoc />
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
-            if (buffer == null)
-            {
-                throw new ArgumentNullException(nameof(buffer));
-            }
-
-            if (offset < 0 || count < 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            if (offset + count > buffer.Length)
-            {
-                throw new ArgumentException();
-            }
-
-            count = (int)Math.Min(count, _remainingBytes);
-
+            count = BytesToRead(buffer, offset, count);
             if (count <= 0)
             {
                 return 0;
             }
-
             int bytesRead = await _underlyingStream.ReadAsync(buffer, offset, count).ConfigureAwait(false);
             _remainingBytes -= bytesRead;
             return bytesRead;
         }
-
         /// <inheritdoc />
         public override int Read(byte[] buffer, int offset, int count)
+        {
+            count = BytesToRead(buffer, offset, count);
+            if (count <= 0)
+            {
+                return 0;
+            }
+            int bytesRead = _underlyingStream.Read(buffer, offset, count);
+            _remainingBytes -= bytesRead;
+            return bytesRead;
+        }
+        private int BytesToRead(byte[] buffer, int offset, int count)
         {
             if (buffer == null)
             {
                 throw new ArgumentNullException(nameof(buffer));
             }
-
             if (offset < 0 || count < 0)
             {
                 throw new ArgumentOutOfRangeException();
             }
-
             if (offset + count > buffer.Length)
             {
                 throw new ArgumentException();
             }
-
             count = (int)Math.Min(count, _remainingBytes);
-
-            if (count <= 0)
-            {
-                return 0;
-            }
-
-            int bytesRead = _underlyingStream.Read(buffer, offset, count);
-            _remainingBytes -= bytesRead;
-            return bytesRead;
+            return count;
         }
-
         /// <inheritdoc />
         public override long Seek(long offset, SeekOrigin origin)
         {
@@ -148,7 +101,6 @@ namespace UiPath.CoreIpc
             {
                 throw new NotSupportedException();
             }
-
             // Recalculate offset relative to the current position
             long newOffset = origin switch
             {
@@ -157,34 +109,22 @@ namespace UiPath.CoreIpc
                 SeekOrigin.Begin => offset - Position,
                 _ => throw new ArgumentOutOfRangeException(nameof(origin)),
             };
-
             // Determine whether the requested position is within the bounds of the stream
             if (Position + newOffset < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(offset));
             }
-
             long currentPosition = _underlyingStream.Position;
             long newPosition = _underlyingStream.Seek(newOffset, SeekOrigin.Current);
             _remainingBytes -= newPosition - currentPosition;
             return Position;
         }
-
         /// <inheritdoc />
         public override void SetLength(long value) => throw new NotSupportedException();
-
         /// <inheritdoc />
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            throw new NotSupportedException();
-        }
-
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => throw new NotSupportedException();
         /// <inheritdoc />
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            throw new NotSupportedException();
-        }
-
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
         /// <inheritdoc />
         protected override void Dispose(bool disposing)
         {
