1e4249730843b61c9051c0d40c028ec27d9e9348
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2021-07-25 20:46:28 +0300

    separate computing and system tests

--- src/UiPath.CoreIpc.Tests/TcpTests..cs
+++ src/UiPath.CoreIpc.Tests/TcpTests..cs
@@ -6,44 +6,21 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Nito.AsyncEx;
 using Shouldly;
 using UiPath.CoreIpc.Tcp;
 using Xunit;
 
 namespace UiPath.CoreIpc.Tests
 {
-    public class TcpTests : IDisposable
+    public class SystemTcpTests : TestBase
     {
-        static readonly IPEndPoint Any = new(IPAddress.Any, 0);
-        static readonly IPEndPoint ComputingEndPoint = new(IPAddress.Loopback, 2121);
         static readonly IPEndPoint SystemEndPoint = new(IPAddress.Loopback, 3131);
-        private const int MaxReceivedMessageSizeInMegabytes = 1;
-        private static readonly TimeSpan RequestTimeout = TimeSpan.FromSeconds(2);
-        private readonly ServiceHost _computingHost;
         private readonly ServiceHost _systemHost;
-        private readonly IComputingService _computingClient;
         private readonly ISystemService _systemClient;
-        private readonly ComputingService _computingService;
         private readonly SystemService _systemService;
-        private readonly ComputingCallback _computingCallback;
-        private readonly IServiceProvider _serviceProvider;
-        private readonly AsyncContext _guiThread = new AsyncContextThread().Context;
-
-        public TcpTests()
+        public SystemTcpTests()
         {
-            _guiThread.SynchronizationContext.Send(() => Thread.CurrentThread.Name = "GuiThread");
-            _computingCallback = new ComputingCallback { Id = System.Guid.NewGuid().ToString() };
-            _serviceProvider = IpcHelpers.ConfigureServices();
-            _computingService = (ComputingService)_serviceProvider.GetService<IComputingService>();
             _systemService = (SystemService)_serviceProvider.GetService<ISystemService>();
-            _computingHost = new ServiceHostBuilder(_serviceProvider)
-                .UseTcp(new TcpSettings(ComputingEndPoint)
-                {
-                    RequestTimeout = RequestTimeout,
-                })
-                .AddEndpoint<IComputingService, IComputingCallback>()
-                .ValidateAndBuild();
             _systemHost = new ServiceHostBuilder(_serviceProvider)
                 .UseTcp(new TcpSettings(SystemEndPoint)
                 {
@@ -53,39 +30,118 @@ namespace UiPath.CoreIpc.Tests
                 })
                 .AddEndpoint<ISystemService>()
                 .ValidateAndBuild();
-
-            var taskScheduler = _guiThread.Scheduler;
-            _computingHost.RunAsync(taskScheduler);
-            _systemHost.RunAsync(taskScheduler);
-            _computingClient = ComputingClientBuilder(taskScheduler).ValidateAndBuild();
+            _systemHost.RunAsync(GuiScheduler);
             _systemClient = CreateSystemService();
         }
-
-        private TcpClientBuilder<IComputingService, IComputingCallback> ComputingClientBuilder(TaskScheduler taskScheduler = null) =>
-            new TcpClientBuilder<IComputingService, IComputingCallback>(ComputingEndPoint, _serviceProvider)
-                .RequestTimeout(RequestTimeout)
-                .CallbackInstance(_computingCallback)
-                .TaskScheduler(taskScheduler);
-
         private ISystemService CreateSystemService(int port = 3131) => SystemClientBuilder(port).ValidateAndBuild();
+
+        public override void Dispose()
+        {
+            ((IDisposable)_systemClient).Dispose();
+            _systemHost.Dispose();
+        }
 
         private TcpClientBuilder<ISystemService> SystemClientBuilder(int port = 3131) =>
             new TcpClientBuilder<ISystemService>(new IPEndPoint(IPAddress.Loopback, port)).RequestTimeout(RequestTimeout).Logger(_serviceProvider);
 
-#if DEBUG
         [Fact]
-        public void MethodsMustReturnTask() => new Action(() => new TcpClientBuilder<IInvalid>(Any).ValidateAndBuild()).ShouldThrow<ArgumentException>().Message.ShouldStartWith("Method does not return Task!");
+        public async Task ConcurrentRequests()
+        {
+            var infinite = _systemClient.Infinite();
+            await Guid();
+            infinite.IsCompleted.ShouldBeFalse();
+        }
+
         [Fact]
-        public void DuplicateMessageParameters() => new Action(() => new TcpClientBuilder<IDuplicateMessage>(Any).ValidateAndBuild()).ShouldThrow<ArgumentException>().Message.ShouldStartWith("The message must be the last parameter before the cancellation token!");
+        public async void BeforeCallServerSide()
+        {
+            var newGuid = System.Guid.NewGuid();
+            var methodName = "";
+            using var protectedService = new ServiceHostBuilder(_serviceProvider)
+                .UseTcp(new TcpSettings(new IPEndPoint(IPAddress.Loopback, 3232)) { RequestTimeout = RequestTimeout })
+                .AddEndpoint(new EndpointSettings<ISystemService>
+                {
+                    BeforeCall = async (call, ct) =>
+                    {
+                        methodName = call.MethodName;
+                        call.Arguments[0].ShouldBe(newGuid);
+                    }
+                })
+                .ValidateAndBuild();
+            _ = protectedService.RunAsync();
+            await CreateSystemService(3232).GetGuid(newGuid);
+            methodName.ShouldBe("GetGuid");
+        }
+
         [Fact]
-        public void TheMessageMustBeTheLastBeforeTheToken() => new Action(() => new TcpClientBuilder<IMessageFirst>(Any).ValidateAndBuild()).ShouldThrow<ArgumentException>().Message.ShouldStartWith("The message must be the last parameter before the cancellation token!");
+        public async Task Void()
+        {
+            _systemService.DidNothing = false;
+            await _systemClient.DoNothing();
+            _systemService.DidNothing.ShouldBeFalse();
+            while (!_systemService.DidNothing)
+            {
+                await Task.Delay(1);
+            }
+        }
+
         [Fact]
-        public void CancellationTokenMustBeLast() => new Action(() => new TcpClientBuilder<IInvalidCancellationToken>(Any).ValidateAndBuild()).ShouldThrow<ArgumentException>().Message.ShouldStartWith("The CancellationToken parameter must be the last!");
+        public async Task VoidThreadName()
+        {
+            await _systemClient.VoidThreadName();
+            await _systemClient.GetThreadName();
+            _systemService.ThreadName.ShouldBe("GuiThread");
+        }
+
         [Fact]
-        public void TheCallbackContractMustBeAnInterface() => new Action(() => new TcpClientBuilder<ISystemService, TcpTests>(Any, _serviceProvider).ValidateAndBuild()).ShouldThrow<ArgumentOutOfRangeException>().Message.ShouldStartWith("The contract must be an interface!");
+        public async Task Enum()
+        {
+            var text = await _systemClient.ConvertText("hEllO woRd!", TextStyle.Upper);
+            text.ShouldBe("HELLO WORD!");
+        }
+
         [Fact]
-        public void TheServiceContractMustBeAnInterface() => new Action(() => new ServiceHostBuilder(_serviceProvider).AddEndpoint<TcpTests>().ValidateAndBuild()).ShouldThrow<ArgumentOutOfRangeException>().Message.ShouldStartWith("The contract must be an interface!");
-#endif
+        public async Task MaxMessageSize()
+        {
+            _systemClient.ReverseBytes(new byte[MaxReceivedMessageSizeInMegabytes * 1024 * 1024]).ShouldThrow<Exception>();
+            await Guid();
+        }
+
+        [Fact]
+        public async Task Guid()
+        {
+            var newGuid = System.Guid.NewGuid();
+            var guid = await _systemClient.GetGuid(newGuid);
+            guid.ShouldBe(newGuid);
+        }
+
+        [Fact]
+        public Task LargeMessage() => _systemClient.ReverseBytes(new byte[(int)(0.7 * MaxReceivedMessageSizeInMegabytes * 1024 * 1024)]);
+
+        [Fact]
+        public async Task ReverseBytes()
+        {
+            var input = Encoding.UTF8.GetBytes("Test");
+            var reversed = await _systemClient.ReverseBytes(input);
+            reversed.ShouldBe(input.Reverse());
+        }
+
+        [Fact]
+        public async Task MissingCallback()
+        {
+            var ex = _systemClient.MissingCallback(new SystemMessage()).ShouldThrow<RemoteException>();
+            ex.Message.ShouldBe("Callback contract mismatch. Requested System.IDisposable, but it's not configured.");
+            ex.Is<ArgumentException>().ShouldBeTrue();
+            await Guid();
+        }
+
+
+        [Fact]
+        public async Task VoidIsAsync() => await _systemClient.VoidSyncThrow();
+
+        [Fact]
+        public async Task GetThreadName() => (await _systemClient.GetThreadName()).ShouldBe("GuiThread");
+
         [Fact]
         public async Task Echo()
         {
@@ -142,18 +198,6 @@ namespace UiPath.CoreIpc.Tests
         }
 
         [Fact]
-        public async Task ReconnectWithEncrypt()
-        {
-            var proxy = ComputingClientBuilder().ValidateAndBuild();
-            for (int i = 0; i < 50; i++)
-            {
-                await proxy.AddFloat(1, 2);
-                ((IpcProxy)proxy).CloseConnection();
-                await proxy.AddFloat(1, 2);
-            }
-        }
-
-        [Fact]
         public async Task DontReconnect()
         {
             var proxy = SystemClientBuilder().DontReconnect().ValidateAndBuild();
@@ -161,14 +205,6 @@ namespace UiPath.CoreIpc.Tests
             ((IpcProxy)proxy).CloseConnection();
             proxy.GetGuid(System.Guid.Empty).ShouldThrow<ObjectDisposedException>();
         }
-
-        [Fact]
-        public async Task AddFloat()
-        {
-            var result = await _computingClient.AddFloat(1.23f, 4.56f);
-            result.ShouldBe(5.79f);
-        }
-
         [Fact]
         public Task CancelServerCall() => CancelServerCallCore(10);
 
@@ -194,6 +230,71 @@ namespace UiPath.CoreIpc.Tests
         }
 
         [Fact]
+        public async Task OptionalMessage()
+        {
+            var returnValue = await _systemClient.ImpersonateCaller();
+            returnValue.ShouldBe(Environment.UserName);
+        }
+
+        [Fact]
+        public async Task ServerTimeout()
+        {
+            var ex = _systemClient.Infinite().ShouldThrow<RemoteException>();
+            ex.Message.ShouldBe($"{nameof(_systemClient.Infinite)} timed out.");
+            ex.Is<TimeoutException>().ShouldBeTrue();
+            await Guid();
+        }
+    }
+    public class ComputingTcpTests : TestBase
+    {
+        static readonly IPEndPoint ComputingEndPoint = new(IPAddress.Loopback, 2121);
+        private readonly ServiceHost _computingHost;
+        private readonly IComputingService _computingClient;
+        private readonly ComputingService _computingService;
+        private readonly ComputingCallback _computingCallback;
+
+        public ComputingTcpTests()
+        {
+            _computingCallback = new ComputingCallback { Id = System.Guid.NewGuid().ToString() };
+            _computingService = (ComputingService)_serviceProvider.GetService<IComputingService>();
+            _computingHost = new ServiceHostBuilder(_serviceProvider)
+                .UseTcp(new TcpSettings(ComputingEndPoint)
+                {
+                    RequestTimeout = RequestTimeout,
+                })
+                .AddEndpoint<IComputingService, IComputingCallback>()
+                .ValidateAndBuild();
+
+            _computingHost.RunAsync(GuiScheduler);
+            _computingClient = ComputingClientBuilder(GuiScheduler).ValidateAndBuild();
+        }
+
+        private TcpClientBuilder<IComputingService, IComputingCallback> ComputingClientBuilder(TaskScheduler taskScheduler = null) =>
+            new TcpClientBuilder<IComputingService, IComputingCallback>(ComputingEndPoint, _serviceProvider)
+                .RequestTimeout(RequestTimeout)
+                .CallbackInstance(_computingCallback)
+                .TaskScheduler(taskScheduler);
+
+        [Fact]
+        public async Task ReconnectWithEncrypt()
+        {
+            var proxy = ComputingClientBuilder().ValidateAndBuild();
+            for (int i = 0; i < 50; i++)
+            {
+                await proxy.AddFloat(1, 2);
+                ((IpcProxy)proxy).CloseConnection();
+                await proxy.AddFloat(1, 2);
+            }
+        }
+
+        [Fact]
+        public async Task AddFloat()
+        {
+            var result = await _computingClient.AddFloat(1.23f, 4.56f);
+            result.ShouldBe(5.79f);
+        }
+
+        [Fact]
         public Task AddFloatConcurrently() => Task.WhenAll(Enumerable.Range(1, 100).Select(_ => AddFloat()));
 
         [Fact]
@@ -211,22 +312,6 @@ namespace UiPath.CoreIpc.Tests
                 _computingClient.Infinite(cancellationSource.Token).ShouldThrow<TaskCanceledException>();
             }
             await AddFloat();
-        }
-
-        [Fact]
-        public async Task OptionalMessage()
-        {
-            var returnValue = await _systemClient.ImpersonateCaller();
-            returnValue.ShouldBe(Environment.UserName);
-        }
-
-        [Fact]
-        public async Task ServerTimeout()
-        {
-            var ex = _systemClient.Infinite().ShouldThrow<RemoteException>();
-            ex.Message.ShouldBe($"{nameof(_systemClient.Infinite)} timed out.");
-            ex.Is<TimeoutException>().ShouldBeTrue();
-            await Guid();
         }
 
         [Fact]
@@ -264,84 +349,7 @@ namespace UiPath.CoreIpc.Tests
         }
 
         [Fact]
-        public async Task ConcurrentRequests()
-        {
-            var infinite = _systemClient.Infinite();
-            await Guid();
-            infinite.IsCompleted.ShouldBeFalse();
-        }
-
-        [Fact]
-        public async Task Void()
-        {
-            _systemService.DidNothing = false;
-            await _systemClient.DoNothing();
-            _systemService.DidNothing.ShouldBeFalse();
-            while (!_systemService.DidNothing)
-            {
-                await Task.Delay(1);
-            }
-        }
-
-        [Fact]
-        public async Task VoidIsAsync() => await _systemClient.VoidSyncThrow();
-
-        [Fact]
-        public async Task GetThreadName() => (await _systemClient.GetThreadName()).ShouldBe("GuiThread");
-
-        [Fact]
         public async Task GetCallbackThreadName() => (await _computingClient.GetCallbackThreadName()).ShouldBe("GuiThread");
-
-        [Fact]
-        public async Task VoidThreadName()
-        {
-            await _systemClient.VoidThreadName();
-            await _systemClient.GetThreadName();
-            _systemService.ThreadName.ShouldBe("GuiThread");
-        }
-
-        [Fact]
-        public async Task Enum()
-        {
-            var text = await _systemClient.ConvertText("hEllO woRd!", TextStyle.Upper);
-            text.ShouldBe("HELLO WORD!");
-        }
-
-        [Fact]
-        public async Task MaxMessageSize()
-        {
-            _systemClient.ReverseBytes(new byte[MaxReceivedMessageSizeInMegabytes * 1024 * 1024]).ShouldThrow<Exception>();
-            await Guid();
-        }
-
-
-        [Fact]
-        public async Task Guid()
-        {
-            var newGuid = System.Guid.NewGuid();
-            var guid = await _systemClient.GetGuid(newGuid);
-            guid.ShouldBe(newGuid);
-        }
-
-        [Fact]
-        public Task LargeMessage() => _systemClient.ReverseBytes(new byte[(int)(0.7 * MaxReceivedMessageSizeInMegabytes * 1024 * 1024)]);
-
-        [Fact]
-        public async Task ReverseBytes()
-        {
-            var input = Encoding.UTF8.GetBytes("Test");
-            var reversed = await _systemClient.ReverseBytes(input);
-            reversed.ShouldBe(input.Reverse());
-        }
-        
-        [Fact]
-        public async Task MissingCallback()
-        {
-            var ex = _systemClient.MissingCallback(new SystemMessage()).ShouldThrow<RemoteException>();
-            ex.Message.ShouldBe("Callback contract mismatch. Requested System.IDisposable, but it's not configured.");
-            ex.Is<ArgumentException>().ShouldBeTrue();
-            await Guid();
-        }
 
         [Fact]
         public Task CallbackConcurrently() => Task.WhenAll(Enumerable.Range(1, 50).Select(_ => Callback()));
@@ -371,34 +379,10 @@ namespace UiPath.CoreIpc.Tests
             error.InnerError.Type.ShouldBe(typeof(InvalidDataException).FullName);
             error.InnerError.Message.ShouldBe("invalid");
         }
-
-        [Fact]
-        public async void BeforeCallServerSide()
-        {
-            var newGuid = System.Guid.NewGuid();
-            var methodName = "";
-            using var protectedService = new ServiceHostBuilder(_serviceProvider)
-                .UseTcp(new TcpSettings(new IPEndPoint(IPAddress.Loopback, 3232)) { RequestTimeout = RequestTimeout })
-                .AddEndpoint(new EndpointSettings<ISystemService>
-                {
-                    BeforeCall = async (call, ct) =>
-                    {
-                        methodName = call.MethodName;
-                        call.Arguments[0].ShouldBe(newGuid);
-                    }
-                })
-                .ValidateAndBuild();
-            _ = protectedService.RunAsync();
-            await CreateSystemService(3232).GetGuid(newGuid);
-            methodName.ShouldBe("GetGuid");
-        }
-        public void Dispose()
+        public override void Dispose()
         {
             ((IDisposable)_computingClient).Dispose();
-            ((IDisposable)_systemClient).Dispose();
             _computingHost.Dispose();
-            _systemHost.Dispose();
-            _guiThread.Dispose();
         }
     }
 }
---
+++ src/UiPath.CoreIpc.Tests/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace UiPath.CoreIpc.Tests
+{
+    public class TestBase : IDisposable
+    {
+        protected const int MaxReceivedMessageSizeInMegabytes = 1;
+        protected static readonly TimeSpan RequestTimeout = TimeSpan.FromSeconds(2);
+        protected readonly IServiceProvider _serviceProvider;
+        protected readonly AsyncContext _guiThread = new AsyncContextThread().Context;
+
+        public TestBase()
+        {
+            _guiThread.SynchronizationContext.Send(() => Thread.CurrentThread.Name = "GuiThread");
+            _serviceProvider = IpcHelpers.ConfigureServices();
+        }
+
+        protected TaskScheduler GuiScheduler => _guiThread.Scheduler;
+
+        public virtual void Dispose()
+        {
+            _guiThread.Dispose();
+        }
+    }
+}
