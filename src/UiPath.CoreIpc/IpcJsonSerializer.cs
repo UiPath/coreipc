800ce2ec80566bdf40a91f177728817346af0602
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2021-08-31 19:27:04 +0300

    cosmetic

--- src/UiPath.CoreIpc/IpcJsonSerializer.cs
+++ src/UiPath.CoreIpc/IpcJsonSerializer.cs
@@ -4,7 +4,6 @@ using System;
 using System.Buffers;
 using System.IO;
 using System.Threading.Tasks;
-
 namespace UiPath.CoreIpc
 {
     public interface ISerializer
@@ -15,16 +14,12 @@ namespace UiPath.CoreIpc
         string Serialize(object obj);
         void Serialize(object obj, Stream stream);
     }
-    class IpcJsonSerializer : ISerializer
+    class IpcJsonSerializer : ISerializer, IArrayPool<char>
     {
-        /// <summary>
-        /// The <see cref="char"/> array pool to use for each <see cref="JsonTextReader"/> instance.
-        /// </summary>
-        static readonly IArrayPool<char> JsonCharArrayPool = new JsonArrayPool<char>(ArrayPool<char>.Shared);
         public object Deserialize(string json, Type type) => JsonConvert.DeserializeObject(json, type);
         public async Task<T> DeserializeAsync<T>(Stream json)
         {
-            var reader = new JsonTextReader(new StreamReader(json)) { ArrayPool = JsonCharArrayPool };
+            var reader = new JsonTextReader(new StreamReader(json)) { ArrayPool = this };
             var jToken = await JToken.LoadAsync(reader);
             return jToken.ToObject<T>();
         }
@@ -38,16 +33,11 @@ namespace UiPath.CoreIpc
         public void Serialize(object obj, Stream stream)
         {
             var serializer = JsonSerializer.CreateDefault();
-            var writer = new JsonTextWriter(new StreamWriter(stream)) { ArrayPool = JsonCharArrayPool };
+            var writer = new JsonTextWriter(new StreamWriter(stream)) { ArrayPool = this };
             serializer.Serialize(writer, obj);
             writer.Flush();
         }
-        class JsonArrayPool<T> : IArrayPool<T>
-        {
-            private readonly ArrayPool<T> _arrayPool;
-            internal JsonArrayPool(ArrayPool<T> arrayPool) => _arrayPool = arrayPool ?? throw new ArgumentNullException(nameof(arrayPool));
-            public T[] Rent(int minimumLength) => _arrayPool.Rent(minimumLength);
-            public void Return(T[] array) => _arrayPool.Return(array ?? throw new ArgumentNullException(nameof(array)));
-        }
+        public char[] Rent(int minimumLength) => ArrayPool<char>.Shared.Rent(minimumLength);
+        public void Return(char[] array) => ArrayPool<char>.Shared.Return(array);
     }
 }
