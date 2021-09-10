0f8c66fa41954a88646afad5ea3287e9aa01a6e4
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2021-09-10 12:07:47 +0300

    more efficient

--- src/UiPath.CoreIpc/IpcJsonSerializer.cs
+++ src/UiPath.CoreIpc/IpcJsonSerializer.cs
@@ -34,7 +34,7 @@ namespace UiPath.CoreIpc
         public object Deserialize(object json, Type type) => json switch
         {
             JToken token => token.ToObject(type, DefaultSerializer),
-            {} => type.IsAssignableFrom(json.GetType()) ? json : JToken.FromObject(json, DefaultSerializer).ToObject(type, DefaultSerializer),
+            { } => type.IsAssignableFrom(json.GetType()) ? json : new JValue(json).ToObject(type),
             null => null,
         };
         public void Serialize(object obj, Stream stream) => Serialize(obj, new StreamWriter(stream));
