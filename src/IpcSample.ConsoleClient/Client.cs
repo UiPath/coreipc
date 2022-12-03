3b38fdfe6b43f2b59e31c7b90d913e3ecd02d38c
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-12-03 10:32:27 +0200

    testing

--- src/IpcSample.ConsoleClient/Client.cs
+++ src/IpcSample.ConsoleClient/Client.cs
@@ -45,7 +45,7 @@ class Client
                 .Logger(serviceProvider)
                 .AllowImpersonation()
                 .ValidateAndBuild();
-            for (int i = 0; i < 5000; i++)
+            for (int i = 0; i < int.MaxValue; i++)
             {
                 // test 1: call IPC service method with primitive types
                 float result1 = await computingClient.AddFloat(1.23f, 4.56f, cancellationToken);
