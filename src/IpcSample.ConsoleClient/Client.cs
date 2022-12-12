19a53af591ef36c35518be6053d44df799945d97
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-12-12 18:50:42 +0200

    p

--- src/IpcSample.ConsoleClient/Client.cs
+++ src/IpcSample.ConsoleClient/Client.cs
@@ -33,6 +33,7 @@ class Client
             .Logger(serviceProvider)
             .AllowImpersonation()
             .ValidateAndBuild();
+        await Calls(computingClient, systemClient, 0, cancellationToken);
         Console.ReadLine();
         var stopwatch = Stopwatch.StartNew();
         int count = 0;
@@ -40,46 +41,7 @@ class Client
         {
             for (int i = 0; i < 50; i++)
             {
-                // test 1: call IPC service method with primitive types
-                float result1 = await computingClient.AddFloat(1.23f, 4.56f, cancellationToken);
-                count++;
-                Console.WriteLine($"[TEST 1] sum of 2 floating number is: {result1}");
-                // test 2: call IPC service method with complex types
-                ComplexNumber result2 = await computingClient.AddComplexNumber(
-                       new ComplexNumber(0.1f, 0.3f),
-                       new ComplexNumber(0.2f, 0.6f), cancellationToken);
-                Console.WriteLine($"[TEST 2] sum of 2 complexe number is: {result2.A}+{result2.B}i");
-
-                // test 3: call IPC service method with an array of complex types
-                ComplexNumber result3 = await computingClient.AddComplexNumbers(new[]
-                {
-                    new ComplexNumber(0.5f, 0.4f),
-                    new ComplexNumber(0.2f, 0.1f),
-                    new ComplexNumber(0.3f, 0.5f),
-                }, cancellationToken);
-                Console.WriteLine($"[TEST 3] sum of 3 complexe number is: {result3.A}+{result3.B}i", cancellationToken);
-
-                // test 4: call IPC service method without parameter or return
-                await systemClient.DoNothing(cancellationToken);
-                Console.WriteLine($"[TEST 4] invoked DoNothing()");
-                //((IDisposable)systemClient).Dispose();
-
-                // test 5: call IPC service method with enum parameter
-                string text = await systemClient.ConvertText("hEllO woRd!", TextStyle.Upper, cancellationToken);
-                Console.WriteLine($"[TEST 5] {text}");
-
-                // test 6: call IPC service method returning GUID
-                Guid generatedId = await systemClient.GetGuid(Guid.NewGuid(), cancellationToken);
-                Console.WriteLine($"[TEST 6] generated ID is: {generatedId}");
-
-                // test 7: call IPC service method with byte array
-                byte[] input = Encoding.UTF8.GetBytes(string.Concat(Enumerable.Range(1, 1).Select(_ => "Test")));
-                byte[] reversed = await systemClient.ReverseBytes(input, cancellationToken);
-                Console.WriteLine($"[TEST 7] reverse bytes");
-
-                // test 8: call IPC service method with callback
-                var userName = await computingClient.SendMessage(new SystemMessage { Text = "client text" }, cancellationToken);
-                Console.WriteLine($"[TEST 8] client identity : {userName}");
+                count = await Calls(computingClient, systemClient, count, cancellationToken);
 
                 //// test 9: call IPC service method with message parameter
                 ////Console.WriteLine($"[TEST 9] callback error");
@@ -109,6 +71,51 @@ class Client
         // test 10: call slow IPC service method
         //await systemClient.SlowOperation(cancellationToken);
         //Console.WriteLine($"[TEST 10] Called slow operation");
+    }
+
+    private static async Task<int> Calls(IComputingService computingClient, ISystemService systemClient, int count, CancellationToken cancellationToken)
+    {
+        // test 1: call IPC service method with primitive types
+        float result1 = await computingClient.AddFloat(1.23f, 4.56f, cancellationToken);
+        count++;
+        Console.WriteLine($"[TEST 1] sum of 2 floating number is: {result1}");
+        // test 2: call IPC service method with complex types
+        ComplexNumber result2 = await computingClient.AddComplexNumber(
+               new ComplexNumber(0.1f, 0.3f),
+               new ComplexNumber(0.2f, 0.6f), cancellationToken);
+        Console.WriteLine($"[TEST 2] sum of 2 complexe number is: {result2.A}+{result2.B}i");
+
+        // test 3: call IPC service method with an array of complex types
+        ComplexNumber result3 = await computingClient.AddComplexNumbers(new[]
+        {
+                    new ComplexNumber(0.5f, 0.4f),
+                    new ComplexNumber(0.2f, 0.1f),
+                    new ComplexNumber(0.3f, 0.5f),
+                }, cancellationToken);
+        Console.WriteLine($"[TEST 3] sum of 3 complexe number is: {result3.A}+{result3.B}i", cancellationToken);
+
+        // test 4: call IPC service method without parameter or return
+        await systemClient.DoNothing(cancellationToken);
+        Console.WriteLine($"[TEST 4] invoked DoNothing()");
+        //((IDisposable)systemClient).Dispose();
+
+        // test 5: call IPC service method with enum parameter
+        string text = await systemClient.ConvertText("hEllO woRd!", TextStyle.Upper, cancellationToken);
+        Console.WriteLine($"[TEST 5] {text}");
+
+        // test 6: call IPC service method returning GUID
+        Guid generatedId = await systemClient.GetGuid(Guid.NewGuid(), cancellationToken);
+        Console.WriteLine($"[TEST 6] generated ID is: {generatedId}");
+
+        // test 7: call IPC service method with byte array
+        byte[] input = Encoding.UTF8.GetBytes(string.Concat(Enumerable.Range(1, 1).Select(_ => "Test")));
+        byte[] reversed = await systemClient.ReverseBytes(input, cancellationToken);
+        Console.WriteLine($"[TEST 7] reverse bytes");
+
+        // test 8: call IPC service method with callback
+        var userName = await computingClient.SendMessage(new SystemMessage { Text = "client text" }, cancellationToken);
+        Console.WriteLine($"[TEST 8] client identity : {userName}");
+        return count;
     }
 
     private static IServiceProvider ConfigureServices() =>
