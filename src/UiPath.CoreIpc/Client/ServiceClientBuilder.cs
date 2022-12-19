8ebcda2a57834a1eef888cbb736f3e5ab46d8777
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-12-19 08:45:24 +0200

    p

--- src/UiPath.CoreIpc/Client/ServiceClientBuilder.cs
+++ src/UiPath.CoreIpc/Client/ServiceClientBuilder.cs
@@ -1,8 +1,6 @@
 ﻿namespace UiPath.CoreIpc;
-
 using ConnectionFactory = Func<Connection, Connection>;
 using BeforeCallHandler = Func<CallInfo, CancellationToken, Task>;
-
 public abstract class ServiceClientBuilder<TDerived, TInterface> where TInterface : class where TDerived : ServiceClientBuilder<TDerived, TInterface>
 {
     protected readonly IServiceProvider _serviceProvider;
@@ -12,45 +10,35 @@ public abstract class ServiceClientBuilder<TDerived, TInterface> where TInterfac
     protected BeforeCallHandler _beforeCall;
     protected object _callbackInstance;
     protected TaskScheduler _taskScheduler;
-
     protected ServiceClientBuilder(Type callbackContract, IServiceProvider serviceProvider)
     {
         CallbackContract = callbackContract;
         _serviceProvider = serviceProvider;
     }
-
     internal Type CallbackContract { get; }
-
     public TDerived DontReconnect() => ConnectionFactory(connection => connection);
-
     public TDerived ConnectionFactory(ConnectionFactory connectionFactory)
     {
         _connectionFactory = connectionFactory;
         return (TDerived)this;
     }
-
     public TDerived BeforeCall(BeforeCallHandler beforeCall)
     {
         _beforeCall = beforeCall;
         return (TDerived)this;
     }
-
     public TDerived Logger(ILogger logger)
     {
         _logger = logger;
         return (TDerived)this;
     }
-
     public TDerived Logger(IServiceProvider serviceProvider) => Logger(serviceProvider.GetRequiredService<ILogger<TInterface>>());
-
     public TDerived RequestTimeout(TimeSpan timeout)
     {
         _requestTimeout = timeout;
         return (TDerived) this;
     }
-
     protected abstract TInterface BuildCore(EndpointSettings serviceEndpoint);
-
     public TInterface Build()
     {
         if (CallbackContract == null)
@@ -64,16 +52,4 @@ public abstract class ServiceClientBuilder<TDerived, TInterface> where TInterfac
         return BuildCore(new(CallbackContract, _callbackInstance) { Scheduler = _taskScheduler, ServiceProvider = _serviceProvider });
     }
 }
-
-public readonly struct CallInfo
-{
-    public CallInfo(bool newConnection, MethodInfo method, object[] arguments)
-    {
-        NewConnection = newConnection;
-        Method = method;
-        Arguments = arguments;
-    }
-    public bool NewConnection { get; }
-    public MethodInfo Method { get; }
-    public object[] Arguments { get; }
-}
+public readonly record struct CallInfo(bool NewConnection, MethodInfo Method, object[] Arguments);
