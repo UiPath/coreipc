461d9c6ccbc46e538d6095a725ead7691fc71f99
Eduard Florin Dumitru <eduard.dumitru@uipath.com>
2024-02-05 15:41:46 +0100

    fixes after review

--- src/Clients/js/test/dotnet/CoreIpcServer/InteropAddress.ts
+++ src/Clients/js/test/dotnet/CoreIpcServer/InteropAddress.ts
@@ -5,7 +5,7 @@ import { BrowserWebSocketAddress } from '../../../src/web';
 export abstract class InteropAddress {
     public static from(address: Address): InteropAddress {
         switch (
-        assertArgument({ address }, BrowserWebSocketAddress, NamedPipeAddress)
+            assertArgument({ address }, BrowserWebSocketAddress, NamedPipeAddress)
         ) {
             case BrowserWebSocketAddress: {
                 return new InteropAddress.WebSocket(
@@ -23,53 +23,21 @@ export abstract class InteropAddress {
         }
     }
 
-    public static computeCommandLineArgs(interopAddresses: InteropAddress[]): string[] {
-        return interopAddresses.flatMap(x => x.commandLineArgs());
-    }
+    public static computeCommandLineArgs(interopAddresses: InteropAddress[]): string[] { return interopAddresses.flatMap(x => x.commandLineArgs()); }
 
     public abstract commandLineArgs(): string[];
-
-    public get underlyingAddress(): Address {
-        return this.getUnderlyingAddress();
-    }
-
-    protected abstract getUnderlyingAddress(): Address;
 }
 
 export module InteropAddress {
     export class WebSocket extends InteropAddress {
-        constructor(private readonly _underlyingAddress: BrowserWebSocketAddress) {
-            super();
-        }
+        constructor(private readonly _underlyingAddress: BrowserWebSocketAddress) { super(); }
 
-        public override commandLineArgs() {
-            return ['--websocket', this._underlyingAddress.url];
-        }
-
-        public get underlyingAddress(): BrowserWebSocketAddress {
-            return this._underlyingAddress;
-        }
-
-        protected override getUnderlyingAddress(): Address {
-            return this._underlyingAddress;
-        }
+        public override commandLineArgs() { return ['--websocket', this._underlyingAddress.url]; }
     }
 
     export class NamedPipe extends InteropAddress {
-        constructor(private readonly _underlyingAddress: NamedPipeAddress) {
-            super();
-        }
+        constructor(private readonly _underlyingAddress: NamedPipeAddress) { super(); }
 
-        public override commandLineArgs() {
-            return ['--pipe', this._underlyingAddress.name];
-        }
-
-        public get underlyingAddress(): NamedPipeAddress {
-            return this._underlyingAddress;
-        }
-
-        protected override getUnderlyingAddress(): Address {
-            return this.underlyingAddress;
-        }
+        public override commandLineArgs() { return ['--pipe', this._underlyingAddress.name]; }
     }
 }
