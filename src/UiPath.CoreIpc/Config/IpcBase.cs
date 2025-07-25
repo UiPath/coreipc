8c0e07d61c1b05249f27bd2d34605cf8e310e04f
Eduard Dumitru <eduard.dumitru@uipath.com>
2025-07-25 18:44:56 +0200

    docs

--- src/UiPath.CoreIpc/Config/IpcBase.cs
+++ src/UiPath.CoreIpc/Config/IpcBase.cs
@@ -2,6 +2,9 @@
 
 public abstract class IpcBase
 {
+    /// <summary>
+    /// The optional default timeout for all invocation requests. Leaving it or explicitly setting it to <c>null</c> has the same effect as setting it to <see cref="Timeout.InfiniteTimeSpan"/>.
+    /// </summary>
     public TimeSpan? RequestTimeout { get; set; }
     public IServiceProvider? ServiceProvider { get; set; }
     public TaskScheduler? Scheduler { get; set; }
